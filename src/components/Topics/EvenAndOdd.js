8dbcd8032da1daad1f334ccb9c05468519cd2446
loren pabst <loren pabst>
2017-06-12 13:47:28 -0600

    added methods to even and odd toy problem

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -2,10 +2,35 @@ import React, {Component} from 'react';
 
 class EvenAndOdd extends Component{
 
+    constructor(){
+        super();
+
+        this.state = {
+            evenArray = [],
+            oddArray = [],
+            userInput = ''
+        }
+    }
+
+    updateUserInput = (val) => {
+        this.setState({
+            userInput: val
+        })
+    }
+
+    solveToyProblem = () => {
+
+    }
 
     render(){
         return (
-            <p>even and odd</p>
+            <div className='puzzleBox evenAndOddPB'>   
+                <h4>Evens and Odds</h4>
+                <input className='inputLine' onChange={ (e) => this.updateUserInput(e.target.value) } />
+                <button className='confirmationButton' onClick={ () => this.solveToyProblem() }></button>
+                <span className='resultsBox'>{ this.state.evenArray }</span>
+                <span className='resultsBox'>{ this.state.oddArray }</span>
+            </div>
         );
     }
 }
