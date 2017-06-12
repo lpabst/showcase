658f8f7155824320cb374eb28d5505c02e796ad5
loren pabst <loren pabst>
2017-06-12 13:37:11 -0600

    added components for topics

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class EvenAndOdd extends Component{
+
+
+    render(){
+        return (
+            <p>even and odd</p>
+        );
+    }
+}
+
+export default EvenAndOdd;
